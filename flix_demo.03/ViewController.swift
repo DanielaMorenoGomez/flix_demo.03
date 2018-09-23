48b1f80be4828088a12a92b038fec35d1f1dbfbe
DanielaMorenoGomez <dmorenogomez1@css.edu>
2018-09-23 08:51:00 +0800

    Initial Commit

---
+++ flix_demo.03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flix_demo.03
+//
+//  Created by user142823 on 9/23/18.
+//  Copyright © 2018 DanielaMoreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
